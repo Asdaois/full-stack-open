87a5fa1d4035c74d092b918acad1bd2edbf538bd
Jose Guevara <josexx1234@gmail.com>
2022-01-09 09:06:59 -0400

    feat(server): initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Notes from 'apps/notes';
-import ThePhonebook from 'apps/the-phonebook';
 
 const App = () => {
   return (
     <div className="box-border p-4">
       <Notes />
-      <ThePhonebook/>
     </div>
   );
 };
