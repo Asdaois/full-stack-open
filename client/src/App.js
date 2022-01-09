a7d8271c450b1565dcb7a64d629845769d65e383
Jose Guevara <josexx1234@gmail.com>
2022-01-09 16:07:55 -0400

    feat: configure deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Notes from 'apps/notes';
+import ThePhonebook from 'apps/the-phonebook';
 
 const App = () => {
   return (
     <div className="box-border p-4">
       <Notes />
+      <ThePhonebook />
     </div>
   );
 };
