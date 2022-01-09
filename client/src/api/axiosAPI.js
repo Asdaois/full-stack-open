6614a3f6798f6488abc416133c5a97c1f4a20d36
Jose Guevara <josexx1234@gmail.com>
2022-01-09 15:17:04 -0400

    fix(backend deploy):

--- client/src/api/axiosAPI.js
+++ client/src/api/axiosAPI.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API; 
+const baseURL = process.env.REACT_APP_API + 'api'; 
 
 const axiosAPI = axios.create({ baseURL});
 axiosAPI.defaults.headers.post["Content-Type"] = "application/json";
-console.log(baseURL);
+
 export default axiosAPI;
