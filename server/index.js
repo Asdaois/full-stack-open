8e4efe6d56b8bbd04b8b5eaa94eaf26115dbe08a
Jose Guevara <josexx1234@gmail.com>
2022-01-09 13:51:52 -0400

    feat(phonebook): create server endpoints

--- server/index.js
+++ server/index.js
@@ -1,45 +1,52 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+// app.use((req, res, next) => {
+//   next();
+// })
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :body :res[body] :response-time ms'))
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
 });
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
 
-const generateID = () => {
-  const maxID = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+const generateID = (dataDB) => {
+  const maxID = dataDB.length > 0 ? Math.max(...dataDB.map((n) => n.id)) : 0;
   return maxID + 1;
-}
+};
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response) => {
   let status = 201;
-  const body = request.body
+  const body = request.body;
 
   if (!body.content) {
     status = 400;
     return response.status(status).json({
-      error: 'content missing'
-    })
+      error: 'content missing',
+    });
   }
 
   const note = {
-    id: generateID,
+    id: generateID(notes),
     content: body.content,
     important: body.important || false,
-    date: new Date()
-  }
+    date: new Date(),
+  };
 
   notes = notes.concat(note);
 
   response.status(status).json(note);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   let status = 200;
 
   const id = Number(request.params.id);
@@ -53,7 +60,7 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   let status = 200;
 
   const id = Number(request.params.id);
@@ -64,6 +71,73 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(status).end();
 });
 
+app.get('/api/phonebook/persons', (request, response) => {
+  let status = 200;
+
+  response.status(status).json(persons);
+});
+
+app.post('/api/phonebook/persons', (request, response) => {
+  let status = 200;
+  const body = request.body;
+
+  if (!body.number || !body.name) {
+    let errorMessage = [];
+    !body.number && errorMessage.push('number is missing');
+    !body.name && errorMessage.push('name is missing');
+    status = 400;
+    return response.status(status).json({ error: errorMessage.join(', ') });
+  }
+
+  if (
+    persons.findIndex(
+      (person) => person.name.toLowerCase() === body.name.toLowerCase()
+    ) !== -1
+  ) {
+    status = 400;
+    return response.status(status).json({ error: 'name must be unique' });
+  }
+
+  const newPerson = {
+    id: generateID(persons),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
+});
+
+app.get('/api/phonebook/persons/:id', (request, response) => {
+  let status = 200;
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === id);
+
+  if (person) return response.status(status).json(person);
+
+  status = 404;
+  response.status(status).end();
+});
+
+app.delete('/api/phonebook/persons/:id', (request, response) => {
+  let status = 204;
+  const id = Number(request.params.id);
+
+  persons = persons.filter((person) => person.id !== id);
+
+  response.statusMessage = `Person with id:${id} deleted`;
+  response.status(status).end();
+});
+
+app.get('/api/phonebook/info', (request, response) => {
+  const info = {
+    message: `Phonebook has info for ${persons.length} people`,
+    date: new Date().toString(),
+  };
+  response.json(info);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running in port http://localhost:${PORT}`);
@@ -72,20 +146,43 @@ app.listen(PORT, () => {
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
     important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
     important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
     important: true,
+  },
+];
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
 ];
