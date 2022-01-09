0d62fa54126787a3fc613f810fd1d2f93ae36b7d
Jose Guevara <josexx1234@gmail.com>
2022-01-09 15:32:28 -0400

    fix: baseURl axios

--- client/src/api/axiosAPI.js
+++ client/src/api/axiosAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API || 'api'; 
+const baseURL = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API : 'api'; 
 
 const axiosAPI = axios.create({ baseURL});
 axiosAPI.defaults.headers.post["Content-Type"] = "application/json";
